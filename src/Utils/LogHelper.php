e9b86f7da9cc8866efc3cfed6a770b3c992b4b9a
yl <1355470410@qq.com>
2020-08-11 09:47:00 +0800

    chore: 日志辅助类修改

--- src/Utils/LogHelper.php
+++ src/Utils/LogHelper.php
@@ -20,17 +20,59 @@ class LogHelper
     protected static $dir = 'storage/logs/%s';
 
     /**
+     * 调试日志
+     * @param string $name
+     * @param string $msg
+     * @param bool $withRequestMessage
+     * @param mixed ...$params
+     * @return mixed
+     */
+    public static function debug(string $name, string $msg, bool $withRequestMessage = false, ...$params)
+    {
+        // 添加请求消息
+        if ($withRequestMessage) {
+            $msg = static::requestMessage() . PHP_EOL . $msg;
+        }
+
+        return Log::debug($name, $msg, ...$params);
+    }
+
+    /**
+     * 保存
+     * @param string $name
+     * @param string $msg
+     * @param bool $withRequestMessage
+     * @param mixed ...$params
+     * @return mixed
+     */
+    public static function save(string $name, string $msg, bool $withRequestMessage = false, ...$params)
+    {
+        // 添加请求消息
+        if ($withRequestMessage) {
+            $msg = static::requestMessage() . PHP_EOL . $msg;
+        }
+
+        return Log::save($name, $msg, ...$params);
+    }
+
+    /**
      * 异常保存
      * @param string $name
      * @param Throwable $e
+     * @param bool $withRequestMessage
      * @return mixed
      */
-    public static function throwableSave(string $name, Throwable $e)
+    public static function throwableSave(string $name, Throwable $e, bool $withRequestMessage = false)
     {
-        return Log::save(
-            $name,
-            static::requestMessage() . PHP_EOL . $e->getMessage() . PHP_EOL . $e->getTraceAsString()
-        );
+        // 保存消息
+        $msg = $e->getMessage() . PHP_EOL . $e->getTraceAsString();
+
+        // 添加请求消息
+        if ($withRequestMessage) {
+            $msg = static::requestMessage() . PHP_EOL . $msg;
+        }
+
+        return Log::save($name, $msg);
     }
 
     /**
@@ -47,16 +89,20 @@ class LogHelper
     }
 
     /**
-     * 请求信息
+     * 请求消息
      * @return string
      */
     public static function requestMessage()
     {
         $request = ContainerHelper::request();
+        $data = json_encode($request->all(), JSON_UNESCAPED_UNICODE);
         return <<<EOL
-ip: {$request->ip()}
-url: {$request->fullUrl()}
-method: {$request->method()}
+ip : {$request->ip()}
+referer : {$request->server('HTTP_REFERER', '-')}
+user_agent : {$request->server('HTTP_USER_AGENT', '-')}
+url : {$request->fullUrl()}
+method : {$request->method()}
+data : {$data}
 EOL;
     }
 }
